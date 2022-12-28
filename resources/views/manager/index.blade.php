9932e226f8d06cc1685dc38b7cdc3d4ee65b84bf
flaaim <flaaim@list.ru>
2022-12-28 22:07:09 +1000

    add user, manager views

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -1,1 +1,20 @@
-MANAGER PANEL
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-12">
+        <table class="table">
+            <thead>
+                <th>Id</th>
+                <th>Theme</th>
+                <th>Message</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Link</th>
+                <th>Date</th>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,1 +1,28 @@
-USER PANEL
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">
+                Feedback
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    <div class="form-group my-3">
+                        <label for="theme">Theme</label>
+                        <input type="text" id="theme" class="form-control" name="theme">
+                    </div>
+                    <div class="form-group my-3">
+                        <label for="message">Message</label>
+                        <textarea name="message" class="form-control" id="message" cols="30" rows="3"></textarea>
+                    </div>
+                    <div class="form-group my-3">
+                        <input type="file" class="form-control-file" id="file">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
