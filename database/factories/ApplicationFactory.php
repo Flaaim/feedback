5688e78e20240800c8ab997931bc0f5da81e3dfa
flaaim <flaaim@list.ru>
2023-01-08 00:12:57 +1000

    create tests sent notification after create application

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Application>
+ */
+class ApplicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'theme' => fake()->word(),
+            'message' => fake()->sentence(),
+            'file' => fake()->word(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
--- tests/Feature/SaveApplicationTest.php
+++ tests/Feature/SaveApplicationTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ApplicationCreateNotification;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Role;
+use App\Models\Application;
+use App\Events\ApplicationCreate;
 
 class SaveApplicationTest extends TestCase
 {
@@ -28,6 +32,24 @@ class SaveApplicationTest extends TestCase
         $response = $this->actingAs($user)->post('/feedback', ['theme' => 'One', 'message' => 'Hello', 'file' => 'file.xml']);
         $response = $this->actingAs($user)->post('/feedback', ['theme' => 'One', 'message' => 'Hello', 'file' => 'file.xml']);
         $response->assertStatus(302);
+    }
+
+    public function test_send_notification_to_manager_after_create_application()
+    {   
+        Notification::fake();
+        $manager = User::factory()->hasRole(['title' => 'Manager', 'user_id' => User::factory()])->create();
+        $application = Application::factory()->create();
+        event(new ApplicationCreate($application));
+        Notification::assertSentTo([$manager], ApplicationCreateNotification::class);
+    }
+
+    public function test_send_notification_to_user_after_create_application()
+    {
+        Notification::fake();
+        $user = User::factory()->hasRole(['title' => 'User', 'user_id' => User::factory()])->create();
+        $application = Application::factory()->create();
+        event(new ApplicationCreate($application));
+        Notification::assertNotSentTo([$user], ApplicationCreateNotification::class);
     }
 
 }
